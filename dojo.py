a0f74062895cec1ab1ed8ef1a4dd5f2a7392c0a3
Kevin <Kevin>
2017-03-01 17:10:50 +0300

    Created Tests for creating a room and adding a user, and added requirements.txt

---
+++ dojo.py
@@ -0,0 +1,16 @@
+class Dojo(object):
+    def __init__(self):
+        self.total_rooms = []
+        self.offices = []
+        self.livingspaces = []
+        self.total_people = []
+        self.staff = []
+        self.fellows = []
+
+    def create_room(self, room_type, room_name):
+        pass
+
+    def add_person(self, person_name, role, accommodation='No'):
+        pass
+
+
