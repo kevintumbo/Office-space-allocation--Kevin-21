b349bd36c4ce404d346b4e3609357edc9adb31b9
Kevin <Kevin>
2017-03-06 05:57:03 +0300

    modified room class

--- apps/rooms.py
+++ apps/rooms.py
@@ -1,2 +1,19 @@
+class Room(object):
+    def __init__(self, room_name):
+        self.room_name = room_name
+        self.occupants = []
 
+
+class Office(Room):
+    def __init__(self, room_name):
+        super().__init__(room_name)
+
+        self.max = 6
+
+
+class LivingSpace(Room):
+    def __init__(self, room_name):
+        super().__init__(room_name)
+
+        self.max = 4
 
