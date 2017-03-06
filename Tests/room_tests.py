27c0b1fd7d8fc9925ac1e0c7af4d868b751f39ac
Kevin <Kevin>
2017-03-06 05:55:30 +0300

    modified room test functionality

--- Tests/room_tests.py
+++ Tests/room_tests.py
@@ -6,38 +6,41 @@ class TestRoomCreation(unittest.TestCase):
 
     """ Test cases to for successful and failed room creation """
 
-    def setup(self):
-        self.dojo = Dojo()
-
     def test_can_create_office(self):
+        self.dojo = Dojo()
         self.assertEqual(len(self.dojo.offices), 0)
-        self.dojo.create_room('office', 'marylin')
+        self.dojo.create_room('office', ['marylin'])
         self.assertEqual(len(self.dojo.offices), 1, 'An office has not been successfully created!')
 
     def test_can_create_living_space(self):
+        self.dojo = Dojo()
         self.assertEqual(len(self.dojo.living_spaces), 0)
-        self.dojo.create_room('living', 'manson')
+        self.dojo.create_room('living', ['manson'])
         self.assertEqual(len(self.dojo.living_spaces), 1, 'A living space has not been successfully created!')
 
     def test_can_create_multiple_office(self):
+        self.dojo = Dojo()
         self.assertEqual(len(self.dojo.offices), 0)
         self.dojo.create_room('office', ['Texas', 'Alabama', 'Chicago', 'Memphis'])
         self.assertEqual(len(self.dojo.offices), 4, 'offices have not been successfully created!')
 
     def test_can_create_multiple_living_space(self):
+        self.dojo = Dojo()
         self.assertEqual(len(self.dojo.living_spaces), 0)
         self.dojo.create_room('living', ['Kenya', 'Uganda', 'Tanzania', 'Rwanda'])
         self.assertEqual(len(self.dojo.living_spaces), 4, 'living spaces have not been successfully created!')
 
-    def test_for_maximum_living_space_created_are_4(self):
-        self.dojo.create_room('living', ['Kenya', 'Uganda', 'Tanzania', 'Rwanda', 'somalia'])
-        self.assertTrue(len(self.dojo.living_spaces) == 4, 'Cannot create more than 4 living spaces')
-
-    def test_for_maximum_offices_created_are_six(self):
-        self.dojo.create_room('office', ['Texas', 'Alabama', 'Chicago', 'Memphis', 'Calif', 'Miami', 'Vegas'])
-        self.assertTrue(len(self.dojo.offices) == 6, 'Cannot create more than 6 living spaces')
+    def test_cannot_duplicate_room(self):
+        self.dojo = Dojo()
+        self.assertEqual(len(self.dojo.total_rooms), 0)
+        self.total_num_of_rooms = len(self.dojo.total_rooms)
+        self.dojo.create_room('living', ['Kenya'])
+        self.dojo.create_room('office', ['Kenya'])
+        self.assertEqual(len(self.dojo.total_rooms), 1,
+                         'living spaces have not been successfully created!')
 
     def test_cannot_create_room_when_arguments_are_missing(self):
+        self.dojo = Dojo()
         self.total_num_of_rooms = len(self.dojo.total_rooms)
         self.dojo.create_room('office', [])
         self.dojo.create_room('living', [])
@@ -48,6 +51,5 @@ class TestRoomCreation(unittest.TestCase):
         self.assertEqual(len(self.dojo.total_rooms), self.total_num_of_rooms,
                          'A room was created without necessary credentials')
 
-    def tearDown(self):
-        self.dojo.__del__()
+
 
