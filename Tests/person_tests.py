665735aa33d756d21f7b5ecd764c6bb539555905
Kevin <Kevin>
2017-03-07 04:04:43 +0300

    added class attributes fellow and staff

--- Tests/person_tests.py
+++ Tests/person_tests.py
@@ -14,36 +14,39 @@ class TestPersonCreation(unittest.TestCase):
         self.dojo.add_person('John', 'Staff', '')
         self.dojo.add_person('Anne', 'Fellow', 'Y')
         self.dojo.add_person('Steve', 'Fellow', 'N')
-        self.assertEqual(len(self.dojo.total_people), 4, 'Person(s) were not added to the system!')
+        self.assertEqual(len(self.dojo.total_people), 4, 'Person(s) were added to the system!')
+
+    """ Test successful addition of staff"""
 
     def test_can_add_staff(self):
         self.dojo = Dojo()
         self.dojo.create_room("office", ["kenya", "uganda"])
         self.dojo.create_room("living", ["Nairobi", "Kampala"])
-        self.assertEqual(len(self.dojo.staff), 0)
-        self.dojo.add_person('Fred', 'Staff', '')
-        self.assertEqual(len(self.dojo.staff), 1, ' staff member(s) has been added to the system!')
+        self.assertEqual(len(self.dojo.total_people), 0)
+        self.dojo.add_person('Fred', 'Staff')
+        self.assertEqual(len(self.dojo.total_people), 1, ' staff member(s) has been added to the system!')
+
+    """ Test successful addition of fellows"""
 
     def test_can_add_fellow(self):
         self.dojo = Dojo()
         self.dojo.create_room("office", ["kenya", "uganda"])
         self.dojo.create_room("living", ["Nairobi", "Kampala"])
-        self.assertEqual(len(self.dojo.fellows), 0)
+        self.assertEqual(len(self.dojo.total_people), 0)
         self.dojo.add_person('Joe', 'Fellow', 'Y')
-        self.dojo.add_person('James', 'Fellow', '')
+        self.dojo.add_person('James', 'Fellow')
         self.dojo.add_person('Larry', 'Fellow', 'N')
-        self.assertEqual(len(self.dojo.fellows), 3, 'staff member(s) were not added to the system!')
+        self.assertEqual(len(self.dojo.total_people), 3, 'staff member(s) were added to the system!')
+
+    """ Test failure in addition of persons with Missing arguments """
 
     def test_cannot_add_person_with_missing_arguments(self):
         self.dojo = Dojo()
         self.dojo.create_room("office", ["kenya", "uganda"])
         self.dojo.create_room("living", ["Nairobi", "Kampala"])
         self.num_of_people = len(self.dojo.total_people)
-        self.dojo.add_person('Kevin', '', '')
+        self.dojo.add_person('Kevin', '')
         self.dojo.add_person('', '', 'Y')
         self.dojo.add_person('', '', '')
         self.assertEqual(len(self.dojo.total_people), self.num_of_people,
                          'Person(s) created without necessary credentials')
-
-
-
