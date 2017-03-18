658b5a366e5e3c31cb2170c8aa2d37652967177d
Kevin <Kevin>
2017-03-18 23:18:21 +0300

    Added accomodation to class arguments

--- apps/person.py
+++ apps/person.py
@@ -2,17 +2,18 @@ class Person(object):
 
     """ class Room """
 
-    def __init__(self, person_name, role=None):
+    def __init__(self, person_name, accomodation, role=None):
         self.person_name = person_name
         self.role = role
+        self.accomodation = accomodation
 
 
 class Fellow(Person):
 
     """ class Fellow inheriting from parent class Room """
 
-    def __init__(self, person_name):
-        super().__init__(person_name, role="FELLOW")
+    def __init__(self, person_name, accomodation):
+        super().__init__(person_name, accomodation, role="FELLOW")
 
 
 class Staff(Person):
@@ -20,7 +21,7 @@ class Staff(Person):
     """ class Staff inheriting from parent class Room """
 
     def __init__(self, person_name):
-        super().__init__(person_name, role="STAFF")
+        super().__init__(person_name, role="STAFF", accomodation="N")
 
 
 
